d0c1c542c98bc31d3102a91d1231d8a2b0587063
ebbestubbe <stubbe.ebbe@gmail.com>
2023-03-12 10:57:28 +0100

    still missing depth first;

--- main.py
+++ main.py
@@ -1,10 +1,58 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.colors import ListedColormap
+import random
 
-def generate_landscape(characters, size):
-    landscape = np.random.choice(characters, size*size).reshape(size,size)
+
+def generate_landscape_random(characters, size0, size1):
+
+    possibilities = [[characters for _1 in range(size0)] for _2 in range(size1)]
+    choice = [[random.choice(subsublist) for subsublist in sublist] for sublist in possibilities]
+    landscape = np.array(choice)
+    #landscape = np.random.choice(characters, size0*size1).reshape(size0,size1)
     return landscape
+
+
+def generate_landscape_wfc(characters, size0, size1):
+
+    #rules:
+    connections = {
+        "Grass": {
+            "top": ["Grass"],
+            "right": ["Grass"],
+            "bottom": ["Grass"],
+            "left": ["Grass"]
+        },
+        "Water": {
+            "top": ["Water"],
+            "right": ["Water"],
+            "bottom": ["Water"],
+            "left": ["Water"]
+        }
+    }
+
+    wavefunction = [[characters for _1 in range(size0)] for _2 in range(size1)]
+    undetermined = [[len(subsublist)!=1 for subsublist in sublist] for sublist in wavefunction]
+    coords = [[(j,i) for i,subsublist in enumerate(sublist) if undetermined[j][i]] for j,sublist in enumerate(wavefunction)]
+    flat_coords = [item for sublist in coords for item in sublist]
+    point = random.choice(flat_coords)
+    random_x = point[0]
+    random_y = point[1]
+
+    #Now we have a random point that we can choose to do something with:
+    # Assume that any choice is valid
+    # Pick random tile
+    # Visit neighbors, remove tiles that are not valid
+    
+
+
+    # for i in landscape.shape[0]:
+    #     for j in landscape.shape[1]:
+    #         neighbor_0 = landscape[i-1,j]
+    #         neighbor_1 = landscape[i,j+1]
+    #         neighbor_2 = landscape[i+1,j]
+    #         neighbor_3 = landscape[i,j-1]
+
 
 
 def plot_landscape(landscape, characters):
@@ -22,19 +70,68 @@ def plot_landscape(landscape, characters):
     plt.show()
 
 
-def main():
-    size = 4
+def run1():
+    size0 = 4
+    size1 = 4
     characters = {
-        'L': [0,1,0,1],
-        'W': [0,0,1,1],
-        'S': [1,1,0,1],
-        "lava": [1,0,0,1]
+        'Grass': [0,1,0,1],
+        'Water': [0,0,1,1],
+        'Sand': [1,1,0,1],
     }
     
 
-    landscape = generate_landscape(characters=list(characters.keys()), size=size)
+    landscape = generate_landscape_random(characters=list(characters.keys()), size0=size0,size1 = size1)
     print(landscape)
     plot_landscape(landscape,characters)
+
+def depth_first_grid():
+    coords = [[(j,i) for i in range(5)] for j in range(5)]
+
+    j = 3
+    i = 1
+    coords[j][i]
+    to_visit = []
+    neighbor_0 = coords[j-1][i]
+    neighbor_1 = coords[j][i+1]
+    neighbor_2 = coords[j+1][i]
+    neighbor_3 = coords[j][i-1]
+    to_visit.append(neighbor_0)
+    to_visit.append(neighbor_1)
+    to_visit.append(neighbor_2)
+    to_visit.append(neighbor_3)
+    
+    print("f99")
+
+def main():
+    depth_first_grid()
+    # size0 = 2
+    # size1 = 3
+    # info = {
+    #     'Grass': [0,1,0,1],
+    #     'Water': [0,0,1,1],
+    #     'Sand': [1,1,0,1],
+    # }
+    # characters=list(info.keys())
+    # generate_landscape_wfc(characters=characters, size0=size0, size1=size1)
+
+    # print("foo")
+    #run1()
+
+    
+    # size0 = 4
+    # size1 = 5
+    # foo = [[["Water", "Grass"] for _ in range(size0)] for _2 in range(size1)]
+    # foo[1][2].remove("Water")
+    # print(foo)
+
+    # size = 4
+    # characters = {
+    #     'Grass': [0,1,0,1],
+    #     'Water': [0,0,1,1],
+    # }
+    # generate_landscape_wfc(characters=characters, size=size)
+
+
 
 
 if __name__ == '__main__':
